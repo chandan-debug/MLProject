43acd509d78888d452f6115ea85a42fad1d47463
chandan-debug <cmahato1201@gmail.com>
2023-10-11 20:47:43 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->list[str]:
+    '''
+This function will return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj: 
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+          requirements.remove(HYPEN_E_DOT)
+
+    return requirements      
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Chandan',
+author_email='cmahato1201@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+
+)
+
+
